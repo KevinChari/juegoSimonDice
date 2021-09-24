78ec617a708cc3f68494ebd64404ed7027bfd8d9
Kevin Alexander Chari <rleycastillo_1999@hotmail.com>
2021-09-24 18:08:31 -0500

    Al boton empezar Juego se añadio la lógica de Js y le añadimos un poco de estilos al Css

--- juegoSimon.js
+++ juegoSimon.js
@@ -1,19 +1,22 @@
-const celeste = document.getElementById('celeste')
-      const violeta = document.getElementById('violeta')
-      const naranja = document.getElementById('naranja')
-      const verde = document.getElementById('verde')
-      const btnEmpezar = document.getElementById('btnEmpezar')
 
-      class Juego {
-        constructor() {
-          this.inicializar()
-        }
+const celeste = document.getElementById("celeste");
+const violeta = document.getElementById("violeta");
+const naranja = document.getElementById("naranja");
+const verde = document.getElementById("verde");
+const btnEmpezar = document.getElementById("btnEmpezar");
 
-        inicializar() {
-          btnEmpezar.classList.add('hide')
-        }
-      }
 
-      function empezarJuego() {
-        var juego = new Juego()
-      }
+class Juego {
+    constructor() {
+        this.inicializar()
+    }
+
+    inicializar () {
+        btnEmpezar.classList.add("hide");
+    }
+}
+
+function empezarJuego() {
+    var juego = new Juego();
+}
+
