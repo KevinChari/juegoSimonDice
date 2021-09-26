e5da21d6872490794f5947cd5751d78f42f1e030
Kevin Alexander Chari <rleycastillo_1999@hotmail.com>
2021-09-26 18:35:46 -0500

    Juego finalizado con proyecto a mejorarlo

--- juegoSimon.js
+++ juegoSimon.js
@@ -4,11 +4,14 @@ const violeta = document.getElementById("violeta");
 const naranja = document.getElementById("naranja");
 const verde = document.getElementById("verde");
 const btnEmpezar = document.getElementById("btnEmpezar");
-const ULTIMO_NIVEL = 10;
+const ULTIMO_NIVEL = 2;
+
+
 
 
 class Juego {
     constructor() {
+        this.inicializar = this.inicializar.bind(this)
         this.inicializar()
         this.generarSecuencia()
         setTimeout(this.siguienteNivel, 500)
@@ -17,13 +20,21 @@ class Juego {
     inicializar () {
         this.siguienteNivel = this.siguienteNivel.bind(this)
         this.elegirColor = this.elegirColor.bind(this)
-        btnEmpezar.classList.add("hide");
+        this.toggleBtnEmpezar()
         this.nivel = 1
         this.colores = {
           celeste,
           violeta,
           naranja,
           verde  
+        }
+    }
+
+    toggleBtnEmpezar () {
+        if(btnEmpezar.classList.contains("hide")) {
+            btnEmpezar.classList.remove("hide");
+        } else {
+            btnEmpezar.classList.add("hide");
         }
     }
 
@@ -90,7 +101,7 @@ class Juego {
     eliminarEventosClick() {
         this.colores.celeste.removeEventListener("click", this.elegirColor)
         this.colores.verde.removeEventListener("click", this.elegirColor)
-        this.colores.violeta.removeventListener("click", this.elegirColor)
+        this.colores.violeta.removeEventListener("click", this.elegirColor)
         this.colores.naranja.removeEventListener("click", this.elegirColor)
     }
 
@@ -105,15 +116,29 @@ class Juego {
               this.nivel++
               this.eliminarEventosClick()
               if (this.nivel === (ULTIMO_NIVEL + 1)) {
-                  // Ganó
+                  this.ganoElJuego()
               } else {
                   setTimeout(this.siguienteNivel, 1500)
               }
           } 
         } else {
-            // Perdio
+            this.perdioElJuego()
         }
     }
+
+    ganoElJuego() {
+        swal("Platzi", "Felicitaciones, ganaste el juego!", "success")
+        .then(this.inicializar)
+    }
+
+    perdioElJuego() {
+        swal("Platzi", "Lo lamentamos, perdiste :(", "error")
+        .then(() => {
+            this.eliminarEventosClick()
+            this.inicializar()
+        })
+    }
+
 }
 
 function empezarJuego() {
